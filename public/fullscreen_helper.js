4ce25425d8aa51182b18dd10aa114f8ee0c2e156
IanLondon <ianlondondu@gmail.com>
2016-07-13 14:47:38 -0400

    fullscreen on desktop firefox

---
+++ public/fullscreen_helper.js
@@ -0,0 +1,29 @@
+var requestFullscreen = function (ele) {
+    if (ele.requestFullscreen) {
+        ele.requestFullscreen();
+    } else if (ele.webkitRequestFullscreen) {
+        ele.webkitRequestFullscreen();
+    } else if (ele.mozRequestFullScreen) {
+        ele.mozRequestFullScreen();
+    } else if (ele.msRequestFullscreen) {
+        ele.msRequestFullscreen();
+    } else {
+        // Fallback
+        console.log('Fullscreen API is not supported.');
+    }
+};
+
+var exitFullscreen = function () {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    } else {
+        // Fallback
+        console.log('Fullscreen API is not supported.');
+    }
+};
